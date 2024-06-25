9f6368dcc36e2c585e554d09253315b63105d5aa
devops-zypl <devops@zypl.ai>
2024-06-25 18:47:00 +0300

    changed docker image backup script

--- backups/docker_image_backups.sh
+++ backups/docker_image_backups.sh
@@ -1,6 +1,7 @@
 docker_image="zypl2registry.azurecr.io/spitamen-scor-api:latest"
 image_name=$(echo $docker_image | cut -d "/" -f 2 | cut -d ":" -f 1)
+echo $image_name
 
-
-docker pull $docker_image
-docker save -o /home/zypl4admin/docker_images/$image_name.tar $docker_image
+/usr/bin/docker pull $docker_image
+/usr/bin/docker save -o /home/sohibjon/devops/$image_name.tar $docker_image
+echo "backup finished"
